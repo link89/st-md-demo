5ed9ac0a50ef0fe993e3884ffc7dff3b02b163e9
zllin <mark@xmu.edu.cn>
2024-09-06 11:36:01 +0800

    feat: add DEFAULT_MATRIX for init viewer

--- demo.py
+++ demo.py
@@ -9,11 +9,15 @@ with open("./template.html", "r") as f:
 
 ngl_html = Template(nglviewer_template).substitute(
         STRUCTURE_URL='/app/static/dump.pdb',
-        TRAJ_URL='/app/static/dump.nc',
+        TRAJ_URL='',
         BACKGROUND_COLOR='#ffffff',
         BODY_STYLE='box-shadow: 0 0 1px 1px #eee;padding: 0 8px;background-color:#fff',
         AUTO_PLAY='false', # true | false
         PLAY_MODE='loop', # loop | once
+        DEFAULT_MATRIX='[]' # Ref: https://threejs.org/docs/#api/en/math/Matrix4
+        # DEFAULT_MATRIX='[2,0,0,0, 0,2,0,0, 0,0,2,0, 0,0,0,1]'
+        # DEFAULT_MATRIX='[1,0,0,0, 0,0.5,0.866,0, 0,-0.866,0.5,0, 0,0,0,1]'
+        # DEFAULT_MATRIX='[1,0,0,0, 0,1,0,0, 0,0,1,0, 10,0,10,1]'
     )
 
 st_components.html(ngl_html, height=600)
