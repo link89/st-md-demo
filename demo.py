f1bf18af3fa4877a26dba7322eef80ab684aa5ac
zllin <mark@xmu.edu.cn>
2024-09-30 11:56:27 +0800

    feat: add REPRESENTATIONS param for custom view

--- demo.py
+++ demo.py
@@ -14,10 +14,20 @@ ngl_html = Template(nglviewer_template).substitute(
         BODY_STYLE='box-shadow: 0 0 1px 1px #eee;padding: 0 8px;background-color:#fff',
         AUTO_PLAY='false', # true | false
         PLAY_MODE='loop', # loop | once
-        DEFAULT_MATRIX='[]' # Ref: https://threejs.org/docs/#api/en/math/Matrix4
+        DEFAULT_MATRIX='[]', # Ref: https://threejs.org/docs/#api/en/math/Matrix4
         # DEFAULT_MATRIX='[2,0,0,0, 0,2,0,0, 0,0,2,0, 0,0,0,1]'
         # DEFAULT_MATRIX='[1,0,0,0, 0,0.5,0.866,0, 0,-0.866,0.5,0, 0,0,0,1]'
         # DEFAULT_MATRIX='[1,0,0,0, 0,1,0,0, 0,0,1,0, 10,0,10,1]'
+        REPRESENTATIONS='''[
+          {"type": "ball+stick", "params": {"sele": "_li", "radiusType": "covalent", "radiusScale": 0.2,}}, 
+          {"type": "licorice", "params": {"sele": "not(_li)"}}
+        ]
+        '''
+        # REPRESENTATIONS='''[
+        #   {"type": "spacefill", "params": {"sele":"not(_AA)","radiusScale":0.5,"radiusSize":0.5,"radiusType":"covalent"}},
+        #   {"type": "licorice", "params": {"sele":"not(_AA)","multipleBond":"symmetric","radiusScale":0.6}},
+        # ]
+        # '''
     )
 
 st_components.html(ngl_html, height=600)
